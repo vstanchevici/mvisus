d395eaffb5003be98d583602849ac6f4c8c0c1b4
Victor Stanchevici <victor@xuver-services.com>
2021-01-04 13:37:33 +0200

    Setting cmake configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
+#include <mvisus-version.h>
 #include <Core/App.h>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!";
+    std::cout << "Project Name:" << MVISUS_PROJECT_NAME;
     App app;
     app.Run();
     return 0;
