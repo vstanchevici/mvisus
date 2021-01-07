62c1fb5da7642ccbdabd84bc66f6b759ff868a42
Victor Stanchevici <victor@xuver-services.com>
2021-01-07 15:19:13 +0200

    Adding to cmake config OpenBLAS and Armadillo library

---
+++ source/Core/MLTest.h
@@ -0,0 +1,25 @@
+#ifndef _ML_TEST_H_
+#define _ML_TEST_H_
+
+namespace mv
+{
+	/*!
+	*	\brief MLTest is a class
+	*/
+	class MLTest
+	{
+		public:
+			MLTest();
+			~MLTest();
+
+		private:
+			int a;
+
+		public:
+			void Run();
+
+	};
+
+}
+
+#endif
--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,29 @@
 #include <mvisus-version.h>
-#include <Core/App.h>
+#include <Core/MLTest.h>
 #include <iostream>
+
+#include <openblas/cblas.h>
+
+void test_open_blas()
+{
+    int i = 0;
+    double A[6] = { 1.0,2.0,1.0,-3.0,4.0,-1.0 };
+    double B[6] = { 1.0,2.0,1.0,-3.0,4.0,-1.0 };
+    double C[9] = { .5,.5,.5,.5,.5,.5,.5,.5,.5 };
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, 3, 3, 2, 1, A, 3, B, 3, 2, C, 3);
+
+    for (i = 0; i < 9; i++)
+        std::cout << C[i] << " ";
+
+    std::cout << std::endl;
+}
+
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Project Name: " << MVISUS_PROJECT_DESCRIPTION;
-    mv::App app;
+    std::cout << "!Project Name: " << MVISUS_PROJECT_DESCRIPTION << std::endl;
+    mv::MLTest app;
     app.Run();
+    //test_open_blas();
     return 0;
 }
