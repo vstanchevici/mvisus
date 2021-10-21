516b34884c66bf64679ae15c64c1ba7f0c99ed68
Victor Stanchevici <vstanchevici@yahoo.com>
2021-10-21 20:58:29 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,40 @@
 #include <MLTest.h>
 #include <iostream>
 
+//https://www.boost.org/doc/libs/1_75_0/libs/serialization/doc/tutorial.html
+// include headers that implement a archive in simple text format
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+/////////////////////////////////////////////////////////////
+// gps coordinate
+//
+// illustrates serialization for a simple type
+//
+class gps_position
+{
+private:
+    friend class boost::serialization::access;
+    // When the class Archive corresponds to an output archive, the
+    // & operator is defined similar to <<.  Likewise, when the class Archive
+    // is a type of input archive the & operator is defined similar to >>.
+    template<class Archive>
+    void serialize(Archive& ar, const unsigned int version)
+    {
+        ar& degrees;
+        ar& minutes;
+        ar& seconds;
+    }
+    int degrees;
+    int minutes;
+    float seconds;
+public:
+    gps_position() {};
+    gps_position(int d, int m, float s) :
+        degrees(d), minutes(m), seconds(s)
+    {}
+};
+
 //#include <openblas/cblas.h>
 
 /*
